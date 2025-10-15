d4624ffbdc09fb607102cb33150984bab68da2a4
Nila Wati <nilaxx025@gmail.com>
2025-10-15 19:27:28 +0700

    add images

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Suspense } from "react"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
+  title: "Nila Wati | Karyawan Administrasi & Operasional Toko",
   description: "Created with v0",
   generator: "v0.app",
 }
