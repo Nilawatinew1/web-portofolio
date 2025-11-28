5fd6a7f1c63ea8d4da2c9fbdf02e121ff3bad4d3
Nila Wati <nilaxx025@gmail.com>
2025-11-28 20:52:52 +0700

    update project

--- lib/cv-data.ts
+++ lib/cv-data.ts
@@ -138,7 +138,7 @@ export const cvData = {
     {
       title: "Data Analyst with Excel - Ousean Group",
       summary:
-        "Melakukan pembersihan data, pengolahan data dengan menganalisis statistik deskriptif, visualisasi data dan penyajian laporan",
+        "Melakukan pembersihan data peserta magang, pengolahan data dengan menganalisis statistik deskriptif, visualisasi data dan penyajian laporan",
       image: "/images/final-project-ousean.png",
       alt: "Laporan analisis data peserta magang menggunakan Excel dan visualisasi di PowerPoint",
       link: "https://www.linkedin.com/posts/nila-wati786_analisa-data-activity-7351070489806524418-eGsw?utm_source=share&utm_medium=member_desktop&rcm=ACoAADoZ7oIB21BETzxOr5fzD7OTMxKt_jSYcew",
@@ -146,14 +146,14 @@ export const cvData = {
     {
       title: "Data Visualization with Excel — MySkill",
       summary:
-        "Mengerjakan mini task MySkill tentang visualisasi data menggunakan Microsoft Excel. Membuat berbagai jenis chart seperti column, line, pie, dan combo axis untuk menampilkan data produksi dan pengeluaran ternak secara informatif dan mudah dipahami.",
+        "Memvisualisasikan data untuk menampilkan data produksi dan pengeluaran ternak secara informatif",
       image: "/images/data-visualization-myskill.png",
       alt: "Visualisasi data produksi dan pengeluaran ternak dengan Excel",
       link: "https://www.linkedin.com/posts/nila-wati786_data-visualization-using-excel-activity-7346516008997670913-mzgz?utm_source=share&utm_medium=member_desktop&rcm=ACoAADoZ7oIB21BETzxOr5fzD7OTMxKt_jSYcew",
     },    
     {
       title: "Infografis Daya Saing Tenaga Kerja Indonesia",
-      summary: "Membuat infografis menggunakan Canva dengan penerapan prinsip desain visual. Visualisasi datanya diolah dari Excel untuk menampilkan perbandingan daya saing tenaga kerja Indonesia di ASEAN.",
+      summary: "Membuat infografis menggunakan Canva dengan penerapan prinsip desain untuk menampilkan perbandingan daya saing tenaga kerja Indonesia di ASEAN",
       image: "/images/tenaga-kerja-indonesia.png",
       alt: "Infografis daya saing tenaga kerja Indonesia di kawasan ASEAN",
       link: "https://www.linkedin.com/posts/nila-wati786_halo-ini-infografis-yang-aku-bikin-pakai-activity-7359858757192835072-4b1y?utm_source=share&utm_medium=member_desktop&rcm=ACoAADoZ7oIB21BETzxOr5fzD7OTMxKt_jSYcew"
@@ -161,14 +161,14 @@ export const cvData = {
     {
       title: "Infografis Etika Digital — Canva Camp",
       summary:
-        "Final project dari Canva Camp berupa infografis bertema etika digital yang menekankan pentingnya komunikasi sopan, menghargai hak cipta, dan menjaga privasi di dunia maya.",
+        "Final project dari Canva Camp berupa infografis bertema etika digital",
       alt: "Infografis Etika Digital: Jadi Pengguna Cerdas di Dunia Maya",
       link: "https://www.linkedin.com/posts/nila-wati786_halooo-ini-adalah-final-project-dari-canva-activity-7347804955061211136-nLbA?utm_source=share&utm_medium=member_desktop&rcm=ACoAAFIgAiIB6l1SQkTA4MT6ICC32jAc7SO21hw",
     },
     {
       title: "Dashboard Penjualan Toko Sinar Surya Tahun 2024",
       summary:
-        "Dashboard yang dibuat sepenuhnya dengan Microsot Excel untuk mendemonstrasikan bagaimana data mentah dapat diubah menjadi wawasan yang jelas untuk pengambilan keputusan usaha kecil.",
+        "Dashboard ini bertujuan untuk mendemonstrasikan data-data transaksi jual-beli dapat diubah menjadi wawasan penting dalam pengambilan keputusan usaha",
       image: "/images/dashboard-sinar.jpg",
       alt: "Dashboard",
       link: "https://www.linkedin.com/posts/nila-wati786_end-to-end-worklow-activity-7397501946632265729-B8Og?utm_source=share&utm_medium=member_desktop&rcm=ACoAAFIgAiIB6l1SQkTA4MT6ICC32jAc7SO21hw",
@@ -176,7 +176,7 @@ export const cvData = {
      {
       title: "Video Pegawai Toko - Artificial Intelligence",
       summary:
-        "Video ini menggambarkan pekerjaan saya sehari-hari yang dibuat menggunakan video generatif AI.",
+        "Video ini menggambarkan pekerjaan saya sehari-hari sebagai pegawai yang dibuat menggunakan video generatif AI.",
       image: "/images/generatif-AI.jpg",
       alt: "Generatif",
       link: "https://www.linkedin.com/posts/nila-wati786_baru-coba-membuat-video-menggunakan-ai-untuk-activity-7395339114277007360-m3WT?utm_source=share&utm_medium=member_desktop&rcm=ACoAAFIgAiIB6l1SQkTA4MT6ICC32jAc7SO21hw",
